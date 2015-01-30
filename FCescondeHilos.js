af1e613731c43e2381d3560a40191ebae0447f85
miguel <miguel@guidedelearning.com>
2015-01-30 09:31:38 +0000

    added some threads and comments

--- FCescondeHilos.js
+++ FCescondeHilos.js
@@ -2,7 +2,7 @@
 // @name         FC esconde hilos
 // @namespace    http://www.forocoches.com
 // @version      0.1
-// @description  enter something useful
+// @description  Esconde hilos en FC en funcion del ID
 // @author       You
 // @match        http://www.forocoches.com/foro/forumdisplay.php?f=2
 // @grant        none
@@ -10,20 +10,18 @@
 // ==/UserScript==
 $(function(){
     
-    var hilos = [];
+    var hilos = [4134937, 4130895, 4134948, 4131321, 4133039, 4033003, 4118054, 4133207, 4132625, 4133118, 4112584, 4118449, 4107186, 4107907, 4110748];
     
     $('#threadbits_forum_2 tr').each(function(){
-    	var idHilo = $(this).find('td[id^="td_threadtitle_"]').attr("id");
+        var idHilo = $(this).find('td[id^="td_threadtitle_"]').attr("id");
         idHilo = idHilo.split("_");
         idHilo = idHilo[2];
         for(var i = 0; i < hilos.length; i++)
         {
-        	if(hilos[i] == idHilo)
+            if(hilos[i] == idHilo)
             {
-                console.log($(this));
-            	$(this).hide();
+                $(this).hide();
             }
         }
     });
 });
-
